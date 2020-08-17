70ef3e8d41cf5857a01e6737fac59b7613115789
bst27 <git@roesmann.net>
2020-08-17 21:12:27 +0200

    add minimal working example

--- cmd/sunrise-sunset-api/main.go
+++ cmd/sunrise-sunset-api/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/kelvins/sunrisesunset"
+	"os"
+	"time"
+)
 
+func main() {
+	d, err := time.Parse("2006-01-02", "2020-04-27")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	p := sunrisesunset.Parameters{
+		Latitude:  51.963691,
+		Longitude: 7.610362,
+		UtcOffset: 0,
+		Date:      d,
+	}
+
+	sunrise, sunset, err := p.GetSunriseSunset()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(sunrise, sunset)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bst27/sunrise-sunset-api
 
 go 1.15
 
-require github.com/kelvins/sunrisesunset v0.0.0-20170601204625-14f1915ad4b4 // indirect
+require github.com/kelvins/sunrisesunset v0.0.0-20170601204625-14f1915ad4b4
